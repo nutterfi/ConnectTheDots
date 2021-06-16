5ceb81f9bffc7bd235c69a9a9f6067be178309d8
Nutter Fi <itsnutterfi@gmail.com>
2021-06-15 22:12:19 -0600

    Initial Commit

---
+++ ConnectTheDots/ConnectTheDotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectTheDotsApp.swift
+//  ConnectTheDots
+//
+//  Created by nutterfi on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct ConnectTheDotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
